43662c61e142e5fa5ae93bb8f0581ee55fa0d64e
Darren Vong <d.vongy@gmail.com>
2017-03-22 01:39:01 +0000

    Updated code to use session managed by Flask to store the game state between requests as opposed to ugly globals

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import os
 from random import randrange
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
+app.secret_key = "guess_the_number"
 socketio = SocketIO(app)
-num_to_guess = -1
 
 def start_game():
-    global num_to_guess
     # Generates a random number between 1 and 100. 101 used in the function since 101 itself is not included
-    num_to_guess = randrange(1, 101)
+    session['num_to_guess'] = randrange(1, 101)
     send("Hey, I am thinking of a number between 1 and 100. Type in your guess "+
         "and I'll let you know whether you are right!")
 
@@ -22,20 +21,19 @@ def root():
 @socketio.on("connect")
 def on_connect():
     start_game()
-    print "Client connected!", num_to_guess
+    print "Client connected!", session["num_to_guess"]
 
 @socketio.on("disconnect")
 def on_disconnect():
     print "Client disconnected!"
-    global num_to_guess
-    num_to_guess = -1
+    session.clear()
 
 @socketio.on("message")
 def on_message(msg):
     # for debugging to see if it remained the same throughout connection
-    print num_to_guess
+    print session["num_to_guess"]
     if msg.isnumeric(): # Check whether the user has typed in a number first...
-        if int(msg) == num_to_guess:
+        if int(msg) == session["num_to_guess"]:
             # send is a shorthand function for emitting the "message" event
             # i.e. send(...) is equivalent to emit("message", ...)
             send("You've guessed the correct number! Well done :)")
@@ -43,7 +41,7 @@ def on_message(msg):
         else:
             # Python shorthand for writing if x is true, assign "higher" to the variable higher_to_lower
             # Else, assign "lower" to the variable higher_to_lower
-            higher_or_lower = "higher" if int(msg) < num_to_guess else "lower"
+            higher_or_lower = "higher" if int(msg) < session["num_to_guess"] else "lower"
             send("Unlucky mate. The number I am thinking of is {} than what you thought".format(higher_or_lower))
     else:
         send("Stop sending me dodgy answers bro, I don't know what you're on about...")
